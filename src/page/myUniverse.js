180b6aec0ef852aa38f0c1e85918cb77d86a8934
smarthug <kirklayer@gmail.com>
2022-01-28 15:44:43 +0900

    gui close & in use effect

--- src/page/myUniverse.js
+++ src/page/myUniverse.js
@@ -11,6 +11,7 @@ let renderer, camera, controls;
  */
 // Debug
 const gui = new GUI({ width: 360 })
+gui.close()
 
 // Scene
 const scene = new THREE.Scene()
@@ -131,17 +132,7 @@ const generateGalaxy = () => {
 
 }
 
-generateGalaxy()
 
-gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
-gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy)
-gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
-gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
-gui.add(parameters, 'spin').min(-5).max(5).step(0.001).onFinishChange(generateGalaxy)
-gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy)
-gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy)
-gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
-gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
 
 // scene.add(new THREE.AxesHelper(5))
 
@@ -208,7 +199,20 @@ export default function Main() {
         });
     }
 
-    function sceneInit() { }
+    function sceneInit() {
+
+        generateGalaxy()
+
+        gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
+        gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy)
+        gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
+        gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
+        gui.add(parameters, 'spin').min(-5).max(5).step(0.001).onFinishChange(generateGalaxy)
+        gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy)
+        gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy)
+        gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
+        gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
+    }
 
     function tick() {
         const elapsedTime = clock.getElapsedTime();
