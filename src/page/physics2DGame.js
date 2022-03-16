b31bf1cea4c18cac2e9d82ae058b2ccee2924a46
smarthug <kirklayer@gmail.com>
2022-03-16 11:50:29 +0900

    moving platform made

--- src/page/physics2DGame.js
+++ src/page/physics2DGame.js
@@ -47,23 +47,23 @@ var circleBody = new p2.Body({
 });
 
 // Add a circle shape to the body
-var circleShape = new p2.Circle({ radius: 1 });
-circleBody.addShape(circleShape);
+// var circleShape = new p2.Circle({ radius: 1 });
+// circleBody.addShape(circleShape);
 
-// ...and add the body to the world.
-// If we don't add it to the world, it won't be simulated.
-world.addBody(circleBody);
+// // ...and add the body to the world.
+// // If we don't add it to the world, it won't be simulated.
+// world.addBody(circleBody);
 
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(1,20,20),
-    new THREE.MeshNormalMaterial()
-)
-sphere.position.set(0,10,0)
-scene.add(sphere)
-objectsToUpdate.push({
-    mesh:sphere,
-    body:circleBody
-})
+// const sphere = new THREE.Mesh(
+//     new THREE.SphereGeometry(1,20,20),
+//     new THREE.MeshNormalMaterial()
+// )
+// sphere.position.set(0,10,0)
+// scene.add(sphere)
+// objectsToUpdate.push({
+//     mesh:sphere,
+//     body:circleBody
+// })
 
 // Create an infinite ground plane body
 var groundBody = new p2.Body({
@@ -74,13 +74,102 @@ groundBody.addShape(groundShape);
 world.addBody(groundBody);
 
 const ground = new THREE.Mesh(
-    new THREE.PlaneGeometry(10,10),
+    new THREE.PlaneGeometry(10, 10),
     new THREE.MeshNormalMaterial({
-        side:THREE.DoubleSide
+        side: THREE.DoubleSide
     })
 )
-ground.rotation.x = -Math.PI*0.5
+ground.rotation.x = -Math.PI * 0.5
 scene.add(ground)
+
+
+// Create moving box
+var boxBody = new p2.Body({
+    mass: 1,
+    position: [1, 4]
+}),
+    boxShape = new p2.Box({
+        width: 0.5,
+        height: 0.5,
+        material: new p2.Material()
+    });
+boxBody.addShape(boxShape);
+world.addBody(boxBody);
+
+let box = new THREE.Mesh(
+    new THREE.BoxGeometry(0.5, 0.5, 0.5),
+    new THREE.MeshNormalMaterial()
+)
+box.position.set(1, 4, 0);
+scene.add(box)
+objectsToUpdate.push({
+    mesh: box,
+    body: boxBody
+})
+
+
+
+// Create static platform box
+var platformBody1 = new p2.Body({
+    mass: 0, // static
+    position: [-0.5, 1]
+}),
+    platformShape1 = new p2.Box({
+        width: 3,
+        height: 0.2,
+        material: new p2.Material()
+    });
+platformBody1.addShape(platformShape1);
+world.addBody(platformBody1);
+
+let platform1 = new THREE.Mesh(
+    new THREE.BoxGeometry(3, 0.2, 1),
+    new THREE.MeshNormalMaterial()
+)
+platform1.position.set(0.5, 1, 0);
+scene.add(platform1)
+objectsToUpdate.push({
+    mesh: platform1,
+    body: platformBody1
+})
+
+// Create static platform box
+var platformBody2 = new p2.Body({
+    mass: 0, // static
+    position: [0.5, 2]
+}),
+    platformShape2 = new p2.Box({
+        width: 3,
+        height: 0.2,
+        material: new p2.Material()
+    });
+platformBody2.addShape(platformShape2);
+world.addBody(platformBody2);
+
+let platform2 = new THREE.Mesh(
+    new THREE.BoxGeometry(3, 0.2, 1),
+    new THREE.MeshNormalMaterial()
+)
+platform2.position.set(0.5, 2, 0);
+scene.add(platform2)
+objectsToUpdate.push({
+    mesh: platform2,
+    body: platformBody2
+})
+
+
+
+
+
+var contactMaterial1 = new p2.ContactMaterial(boxShape.material, platformShape1.material, {
+    surfaceVelocity: -0.5,
+});
+world.addContactMaterial(contactMaterial1);
+
+var contactMaterial2 = new p2.ContactMaterial(boxShape.material, platformShape2.material, {
+    surfaceVelocity: 0.5,
+});
+world.addContactMaterial(contactMaterial2);
 
 export default function Main() {
     const canvasRef = useRef();
@@ -117,6 +206,7 @@ export default function Main() {
 
         renderer = new THREE.WebGLRenderer({
             canvas: canvas,
+            antialias:true
         });
         renderer.setSize(sizes.width, sizes.height);
         renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
@@ -156,7 +246,7 @@ export default function Main() {
         for (const object of objectsToUpdate) {
             // object.mesh.position.copy(object.body.position)
             // console.log(object)
-            object.mesh.position.set(object.body.position[0],object.body.position[1],0)
+            object.mesh.position.set(object.body.position[0], object.body.position[1], 0)
             // object.mesh.quaternion.copy(object.body.quaternion)
             // object.mesh.quaternion.copy(object.body.quaternion)
         }
