40be940d7ee32fad40f6fa86fbed95feb7f16725
smarthug <kirklayer@gmail.com>
2022-03-19 15:50:48 +0900

    update physics done

--- src/page/NoPhysicsGame/step1.js
+++ src/page/NoPhysicsGame/step1.js
@@ -48,6 +48,7 @@ let tempSegment = new THREE.Line3();
 const scene = new THREE.Scene()
 
 const clock = new THREE.Clock()
+let oldElapsedTime = 0;
 
 /**
  * Textures
@@ -402,25 +403,194 @@ export default function Main() {
 
     }
 
+    function updatePlayer(delta) {
+
+        playerVelocity.y += playerIsOnGround ? 0 : delta * params.gravity;
+        player.position.addScaledVector(playerVelocity, delta);
+
+        // move the player
+        const angle = controls.getAzimuthalAngle();
+        if (fwdPressed) {
+
+            tempVector.set(0, 0, - 1).applyAxisAngle(upVector, angle);
+            player.position.addScaledVector(tempVector, params.playerSpeed * delta);
+
+        }
+
+        if (bkdPressed) {
+
+            tempVector.set(0, 0, 1).applyAxisAngle(upVector, angle);
+            player.position.addScaledVector(tempVector, params.playerSpeed * delta);
+
+        }
+
+        if (lftPressed) {
+
+            tempVector.set(- 1, 0, 0).applyAxisAngle(upVector, angle);
+            player.position.addScaledVector(tempVector, params.playerSpeed * delta);
+
+        }
+
+        if (rgtPressed) {
+
+            tempVector.set(1, 0, 0).applyAxisAngle(upVector, angle);
+            player.position.addScaledVector(tempVector, params.playerSpeed * delta);
+
+        }
+
+        player.updateMatrixWorld();
+
+        // adjust player position based on collisions
+        const capsuleInfo = player.capsuleInfo;
+        tempBox.makeEmpty();
+        tempMat.copy(collider.matrixWorld).invert();
+        tempSegment.copy(capsuleInfo.segment);
+
+        // get the position of the capsule in the local space of the collider
+        tempSegment.start.applyMatrix4(player.matrixWorld).applyMatrix4(tempMat);
+        tempSegment.end.applyMatrix4(player.matrixWorld).applyMatrix4(tempMat);
+
+        // get the axis aligned bounding box of the capsule
+        tempBox.expandByPoint(tempSegment.start);
+        tempBox.expandByPoint(tempSegment.end);
+
+        tempBox.min.addScalar(- capsuleInfo.radius);
+        tempBox.max.addScalar(capsuleInfo.radius);
+
+        collider.geometry.boundsTree.shapecast({
+
+            intersectsBounds: box => box.intersectsBox(tempBox),
+
+            intersectsTriangle: tri => {
+
+                // check if the triangle is intersecting the capsule and adjust the
+                // capsule position if it is.
+                const triPoint = tempVector;
+                const capsulePoint = tempVector2;
+
+                const distance = tri.closestPointToSegment(tempSegment, triPoint, capsulePoint);
+                if (distance < capsuleInfo.radius) {
+
+                    const depth = capsuleInfo.radius - distance;
+                    const direction = capsulePoint.sub(triPoint).normalize();
+
+                    tempSegment.start.addScaledVector(direction, depth);
+                    tempSegment.end.addScaledVector(direction, depth);
+
+                }
+
+            }
+
+        });
+
+        // get the adjusted position of the capsule collider in world space after checking
+        // triangle collisions and moving it. capsuleInfo.segment.start is assumed to be
+        // the origin of the player model.
+        const newPosition = tempVector;
+        newPosition.copy(tempSegment.start).applyMatrix4(collider.matrixWorld);
+
+        // check how much the collider was moved
+        const deltaVector = tempVector2;
+        deltaVector.subVectors(newPosition, player.position);
+
+        // if the player was primarily adjusted vertically we assume it's on something we should consider ground
+        playerIsOnGround = deltaVector.y > Math.abs(delta * playerVelocity.y * 0.25);
+
+        const offset = Math.max(0.0, deltaVector.length() - 1e-5);
+        deltaVector.normalize().multiplyScalar(offset);
+
+        // adjust the player model
+        player.position.add(deltaVector);
+
+        if (!playerIsOnGround) {
+
+            deltaVector.normalize();
+            playerVelocity.addScaledVector(deltaVector, - deltaVector.dot(playerVelocity));
+
+        } else {
+
+            playerVelocity.set(0, 0, 0);
+
+        }
+
+        // adjust the camera
+        camera.position.sub(controls.target);
+        controls.target.copy(player.position);
+        camera.position.add(player.position);
+
+        // if the player has fallen too far below the level reset their position to the start
+        if (player.position.y < - 25) {
+
+            reset();
+
+        }
+
+    }
+
 
 
     function sceneInit() {
 
         // Debug
-        const gui = new GUI()
+        // const gui = new GUI()
     }
 
     function tick() {
-        const elapsedTime = clock.getElapsedTime();
+        // const elapsedTime = clock.getElapsedTime();
 
-        // Update controls
+        // // Update controls
+        // controls.update();
+
+        // // Render
+        // renderer.render(scene, camera);
+
+        // // Call tick again on the next frame
+        // requestAnimationFrame(tick);
+
+        stats.update();
+        requestAnimationFrame(tick);
+
+        const elapsedTime = clock.getElapsedTime();
+        // const deltaTime = elapsedTime - oldElapsedTime
+        const delta = elapsedTime - oldElapsedTime
+        oldElapsedTime = elapsedTime
+
+        // const delta = Math.min(clock.getDelta(), 0.1);
+        if (params.firstPerson) {
+
+            controls.maxPolarAngle = Math.PI;
+            controls.minDistance = 1e-4;
+            controls.maxDistance = 1e-4;
+
+        } else {
+
+            controls.maxPolarAngle = Math.PI / 2;
+            controls.minDistance = 1;
+            controls.maxDistance = 20;
+
+        }
+
+        if (collider) {
+
+            collider.visible = params.displayCollider;
+            visualizer.visible = params.displayBVH;
+
+            const physicsSteps = params.physicsSteps;
+
+            for (let i = 0; i < physicsSteps; i++) {
+
+                updatePlayer(delta / physicsSteps);
+
+            }
+
+        }
+
+        // TODO: limit the camera movement based on the collider
+        // raycast in direction of camera and move it if it's further than the closest point
+
         controls.update();
 
-        // Render
         renderer.render(scene, camera);
-
-        // Call tick again on the next frame
-        requestAnimationFrame(tick);
     }
 
     return <canvas ref={canvasRef} className="webgl"></canvas>;
